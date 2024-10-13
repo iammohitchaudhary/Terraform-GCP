86bfdb49359be00a8146cc4289a84747c4374d4d
iammohitchaudhary <139449572+iammohitchaudhary@users.noreply.github.com>
2024-10-13 14:18:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,9 @@
 resource "google_storage_bucket" "my-bucket" {
-  name                     = "tt-githubdemo-bucket-000001"
+  name                     = "mohitchaudhary1"
   project                  = "test-terraform-438504"
   location                 = "US"
   force_destroy            = true
   public_access_prevention = "enforced"
 }
 #
-resource "google_storage_bucket" "my-bucket2" {
-  name                     = "tt-githubdemo-bucket-000002"
-  project                  = "test-terraform-438504"
-  location                 = "US"
-  force_destroy            = true
-  public_access_prevention = "enforced"
-}
-#
-### dummy
+
